b538577b338bd25a2b420354adf46cf315b69cc6
DestanyM <destanymcclellan22@gmail.com>
2023-07-28 16:01:46 -0400

    Part 1 and 2 completed, working on the rest

--- src/main/java/rocks/zipcode/io/comparators/IdComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/IdComparator.java
@@ -1,7 +1,21 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class IdComparator {
+public class IdComparator implements Comparator<Item> {
+
+    public int compare(Item a, Item b) {
+        if (a.getId() < b.getId()){
+            return -1;
+        } else if (a.getId() > b.getId()){
+            return 1;
+        }
+        else return 0;
+    }
+
 }
